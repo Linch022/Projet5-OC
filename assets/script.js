8f06e10b3b49a5e448d22d9341c8e7104ba7cd13
Nicolas Berthollet <berthollet.nicolas@gmail.com>
2024-11-19 15:05:21 +0100

    added bullet point and logic to the selected bulletpoint

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,37 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+// Récupération des flèches dans le DOM
+let arrowRight = document.querySelector(".arrow_right");
+let arrowLeft = document.querySelector(".arrow_left");
+
+arrowRight.addEventListener("click", function () {
+	console.log("click droite")
+});
+arrowLeft.addEventListener("click", function () {
+	console.log("click gauche")
+});
+
+
+// Ajout des bullets points et récupération de l'image en cours de visionnage
+let dotsContainer = document.querySelector(".dots");
+let bannerImg = document.querySelector(".banner-img");
+
+for (let i = 0; i < slides.length; i++) {
+	let dot = document.createElement("span");
+	dot.classList.add("dot");
+	if(i === 0) {
+		dot.classList.add("dot_selected");	
+	}
+	dot.id = `dot-${i}`;
+	dotsContainer.appendChild(dot);
+
+	dot.addEventListener("click", function() {
+		bannerImg.src = `./assets/images/slideshow/${slides[i].image}`;
+		const newSelected = document.getElementById(`dot-${i}`);
+		const previousSelected = document.querySelector(".dot_selected");
+		previousSelected.classList.remove("dot_selected");
+		newSelected.classList.add("dot_selected");
+	})
+}
